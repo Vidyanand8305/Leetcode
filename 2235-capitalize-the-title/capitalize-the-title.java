5b8980a723d6ccf41e180071db3084b57396aa79
V Sharma <112870569+Vidyanand8305@users.noreply.github.com>
2026-02-14 22:21:48 +0530

    Time: 3 ms (84.92%) | Memory: 44.2 MB (39.26%) - LeetSync

--- 2235-capitalize-the-title/capitalize-the-title.java
+++ 2235-capitalize-the-title/capitalize-the-title.java
@@ -1,23 +1,22 @@
 class Solution {
     public String capitalizeTitle(String title) {
+
         String[] words = title.split(" ");
-        StringBuilder sb = new StringBuilder();
+        StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < words.length; i++) {
-            String w = words[i];
+            String w = words[i].toLowerCase(); // pehle sab lowercase
 
             if (w.length() <= 2) {
-                sb.append(w.toLowerCase());
+                result.append(w);
             } else {
-                String first = w.substring(0, 1).toUpperCase();
-                String rest = w.substring(1).toLowerCase();
-                sb.append(first + rest);
+                result.append(Character.toUpperCase(w.charAt(0)));
+                result.append(w.substring(1));
             }
 
-            if (i < words.length - 1)
-                sb.append(" ");
+            if (i != words.length - 1) result.append(" ");
         }
 
-        return sb.toString();
+        return result.toString();
     }
 }
