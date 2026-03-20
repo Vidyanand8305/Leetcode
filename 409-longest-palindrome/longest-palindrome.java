c030a19e6e81aea317fd0857bff82ba0f8f08f55
V Sharma <112870569+Vidyanand8305@users.noreply.github.com>
2026-03-20 22:31:41 +0530

    Time: 7 ms (49.74%) | Memory: 43.1 MB (80.92%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -1,32 +1,19 @@
 class Solution {
     public int longestPalindrome(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
 
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-
-            if(map.containsKey(ch)){
-                int oldf = map.get(ch);
-                map.put(ch, oldf + 1);
-            }else{
-                map.put(ch, 1);
-            }
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
+
         int len = 0;
-        boolean oddFound = false;
-        for(char key : map.keySet()){
-            int freq = map.get(key);
-            if(freq % 2 == 0){
-                len = len + freq;
-            }else{
-                len = len + (freq - 1);
-                oddFound = true;
-            }
+        boolean odd = false;
+
+        for (int freq : map.values()) {
+            len += (freq / 2) * 2;
+            if (freq % 2 == 1) odd = true;
         }
 
-        if(oddFound == true){
-            len = len + 1;
-        }
-        return len;
+        return odd ? len + 1 : len;
     }
 }
